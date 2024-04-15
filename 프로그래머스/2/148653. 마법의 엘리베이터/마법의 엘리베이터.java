1bbc6f87f301029d42b2f8d1546f4c2e74003bdf
stein1213 <155411194+stein1213@users.noreply.github.com>
2024-04-15 16:51:11 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        while (storey != 0) {
+            if (storey % 10 < 5) {
+                answer += storey % 10;
+            } else if (storey % 10 == 5) {
+                if(storey % 100 > 50) {
+                    answer += 10 - (storey % 10);
+                    storey += 10;
+                } else {
+                    answer += storey % 10;
+                }
+            }
+            else{
+                answer += 10 - (storey % 10);
+                storey += 10;
+            }
+            storey = storey / 10;
+        }
+        
+        return answer;
+    }
+}
