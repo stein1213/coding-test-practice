6b36fee8444a41487e071634ee8cc37f4cb65185
stein1213 <155411194+stein1213@users.noreply.github.com>
2024-08-07 16:56:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.83 ms, Memory: 96.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < completion.length; i++) {
+            
+            if (map.containsKey(completion[i])) {
+                map.put(completion[i], map.get(completion[i]) + 1);
+            } else {
+                 map.put(completion[i], 1);
+            }
+        }
+        
+        for(int i = 0; i < participant.length; i++) {
+        
+            if (!map.containsKey(participant[i]) || map.get(participant[i]) < 1) {
+                return participant[i];
+            } else {
+                map.put(participant[i], map.get(participant[i]) - 1);
+            }
+        }
+        
+        return "";
+    }
+}
