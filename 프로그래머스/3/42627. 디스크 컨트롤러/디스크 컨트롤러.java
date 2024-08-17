bf4ac72d5948223652216d6c91e56b96eef137e6
stein1213 <155411194+stein1213@users.noreply.github.com>
2024-08-17 21:02:30 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 9.66 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,27 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        int answer = 0;
-        
-        List<int[]> list = new ArrayList<>();
-        
+        // 현재 시각
         int current = 0;
-        
+        // 요청~종료까지의 시간의 총합
         int operationTime = 0;
-        
+        // jobs의 인덱스값
         int index = 0;
         
-        List<int[]> jobList = Arrays.asList(jobs);
-        
-        Collections.sort(jobList, (a, b) -> {
+        // jobs를 요청시간 순으로 정렬
+        Arrays.sort(jobs, (a, b) -> {
             return a[0] - b[0];
         });
         
-        while (list.size() != 0 || index < jobList.size()) {
+        // 실행 대기중인 작업 리스트
+        List<int[]> list = new ArrayList<>();
+        
+        while (list.size() != 0 || index < jobs.length) {
             
-            while (index < jobList.size() && jobList.get(index)[0] <= current) {
+            // current 기준으로 대기리스트에 작업 추가
+            while (index < jobs.length && jobs[index][0] <= current) {
                 
-                list.add(jobList.get(index++));
+                list.add(jobs[index++]);
             }
             
             if (list.size() == 0) {
@@ -30,22 +30,21 @@ class Solution {
                 continue;
             }
             
+            // 대기 리스트를 실행시간 기준으로 정렬
             Collections.sort(list, (a, b) -> {
                 
-                if (a[1] == b[1]) return a[0] - b[0];
                 return a[1] - b[1];
             });
-            
+            // 실행시간이 가장 짧은 작업 실행
             current += list.get(0)[1];
             
             operationTime += current - list.get(0)[0];
             
+            // 완료된 작업 리스트에서 제거
             list.remove(0);
             
         }
         
-        answer = operationTime / jobs.length;
-        
-        return answer;
+        return operationTime / jobs.length;
     }
 }
