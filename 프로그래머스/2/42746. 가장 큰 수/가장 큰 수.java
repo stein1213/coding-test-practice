f2333bbaa8173bcc3e709d4ec8eeaf2920c11527
stein1213 <155411194+stein1213@users.noreply.github.com>
2024-08-17 18:47:34 +0900

    [level 2] Title: 가장 큰 수, Time: 233.45 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -4,50 +4,22 @@ class Solution {
     public String solution(int[] numbers) {
         StringBuffer answer = new StringBuffer();
         
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
         
-       for (int number : numbers) {
-           
-           int length = Integer.toString(number).length();
-           
-           int key = number / (int)(Math.pow(10, length - 1));
-           
-           List<Integer> list = map.getOrDefault(key, new ArrayList<Integer>());
-           
-           list.add(number);
-           
-           map.put(key, list);
-       }
-        
-        Set<Integer> keySet = map.keySet();
-        
-        Iterator itr = keySet.iterator();
-        
-        while(itr.hasNext()) {
-            
-            int key = (Integer)itr.next();
-            
-            List<Integer> list = map.get(key);
-            
-            Collections.sort(list, new MyComparator());
-            
-            map.put(key, list);
+        for(int a : numbers) {
+            list.add(a);
         }
         
-        for(int i = 9; i >= 0; i--) {
+        Collections.sort(list, new MyComparator());
+        
+        for(int a : list) {
             
-            if (!keySet.contains(i)) continue;
-            
-            if (i == 0 && answer.length() == 0) {
+            if (a == 0 && answer.length() == 0) {
                 answer.append(0);
                 break;
             }
-            
-            for(int a : map.get(i)) {
-                
-                answer.append(a);
-            }
-        }
+            answer.append(a);
+        }        
         
         return answer.toString();
         
@@ -66,31 +38,6 @@ class Solution {
             
             return b - a;
             
-//             int lengthDifference = sx.length() - sy.length();
-            
-//             if (lengthDifference > 0) {
-                
-//                 for(int i = 0; i <= lengthDifference; i = i + sy.length()) {
-                    
-//                     int tmp = Integer.parseInt(sx.substring(i, sy.length() + i));
-                    
-//                     if (tmp != y) return y - tmp;
-//                 }
-                
-//                 return -1;
-//             } else if (lengthDifference < 0) {
-                
-//                 for(int i = 0; i <= lengthDifference; i = i + sx.length()) {
-                    
-//                     int tmp = Integer.parseInt(sy.substring(i, sx.length() + i));
-                    
-//                     if (tmp != x) return tmp - x;
-//                 }
-                
-//                 return 1;
-//             } else return y - x;
-            
-            // 1211 121 56 5651
         }
     }
 }
